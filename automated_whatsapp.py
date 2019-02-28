0394e615de2fe2e63d905dee75ba3c574c11ad22
Piyush <piyushranjan072@gmail.com>
2019-02-28 12:36:18 +0530

    v1.0

---
+++ automated_whatsapp.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By
+import pyautogui as pg
+import time
+
+driver = webdriver.Firefox()
+driver.get("https://web.whatsapp.com/")
+target = "'Sushil CSE'"
+xpath = '//span[@title='+target+']'
+driver.find_element(By.XPATH, xpath).click()
+string = 'kaha hai?'
+tb_id = "'_2S1VP copyable-text selectable-text'"
+input_box =  driver.find_element(By.XPATH, '//div[@class='+tb_id+']')
+for i in range(100):
+	input_box.send_keys(string+Keys.ENTER)
